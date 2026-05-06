9a96bbf2e0d6267b2b77fa7b6cce0861b4aee2c4
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2026-05-06 12:20:13 -0400

    Footer desktop established

---
+++ src/twig/components/footer/footer.config.js
@@ -0,0 +1,173 @@
+module.exports = {
+  title: 'Footer',
+  status: 'wip',
+  context: {
+    primary: [
+      {
+        url: '/',
+        title: 'Collective Solutions',
+        children: [
+          {
+            url: '/',
+            title: 'Outreach and Re-Engagement',
+            excerpt: 'Connect and inspire'
+          },
+          {
+            url: '/',
+            title: 'Data',
+            excerpt: 'Insights that drive action'
+          },
+          {
+            url: '/',
+            title: 'Technology',
+            excerpt: 'Custom tools adapted to you'
+          },
+          {
+            url: '/',
+            title: 'Coaching & Learner Support',
+            excerpt: 'Guidance for every step'
+          },
+        ],
+      },
+      {
+        url: '/',
+        title: 'Who Can Benefit',
+        children: [
+          {
+            url: '/',
+            title: 'Re-enroll stop outs',
+            tag: 'Institutions',
+          },
+          {
+            url: '/',
+            title: 'Complete your degree',
+            tag: 'Learners',
+          },
+          {
+            url: '/',
+            title: 'Grow your workforce',
+            tag: 'States',
+          },
+        ],
+      },
+      {
+        url: '/',
+        title: 'Resources',
+        children: [
+          {
+            url: '/',
+            title: 'For Institutions',
+            tag: 'Institutions',
+          },
+          {
+            url: '/',
+            title: 'For Learners',
+            tag: 'Learners',
+          },
+          {
+            url: '/',
+            title: 'For States',
+            tag: 'States',
+          },
+        ],
+      },
+      {
+        url: '/',
+        title: 'Company',
+        children: [
+          {
+            url: '/',
+            title: 'Our Team',
+          },
+          {
+            url: '/',
+            title: 'Careers',
+          },
+          {
+            url: '/',
+            title: 'Press',
+          },
+        ],
+      },
+      {
+        url: '/',
+        title: 'Try Reup',
+        children: [
+          {
+            url: '/',
+            title: 'States',
+          },
+          {
+            url: '/',
+            title: 'Institutions',
+          },
+          {
+            url: '/',
+            title: 'Learners',
+          },
+          {
+            url: '/',
+            title: 'General Inquiries',
+          },
+        ]
+      },
+      {
+        url: '/',
+        title: 'Social',
+        children: [
+          {
+            url: '/',
+            title: 'LinkedIn',
+          },
+          {
+            url: '/',
+            title: 'Facebook',
+          },
+          {
+            url: '/',
+            title: 'Instagram',
+          },
+        ]
+      },
+      {
+        url: '/',
+        title: 'Newsletter',
+        children: [
+          {
+            url: '/',
+            title: 'For Institutions',
+            tag: 'Institutions',
+          },
+          {
+            url: '/',
+            title: 'For Learners',
+            tag: 'Learners',
+          },
+          {
+            url: '/',
+            title: 'For States',
+            tag: 'States',
+          },
+        ],
+      },
+    ],
+    address: {
+      title: '201 West 5th Street, Suite 1450 Austin, TX 78701',
+      url: 'https://www.google.com/maps/place/ReUp/@30.2673745,-97.7450229,16z/data=!4m6!3m5!1s0x8644b5001968c933:0x4a6d9f506e18cbe8!8m2!3d30.2673479!4d-97.7448486!16s%2Fg%2F11z3_2n42_?entry=ttu&g_ep=EgoyMDI2MDUwMi4wIKXMDSoASAFQAw%3D%3D'
+    },
+    copyright: [
+      {
+        url: '/',
+        title: 'All Rights Reserved'
+      },
+      {
+        url: '/',
+        title: 'Terms and Conditions'
+      },
+      {
+        url: '/',
+        title: 'Privacy Policy'
+      },
+    ]
+  }
+}
