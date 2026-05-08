f89f48d0fa565dd552890f3ce2d75bbcf061dd9c
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2026-05-08 10:49:15 -0400

    Tweaks to tagging system. Hero parallel added

--- src/twig/components/hero/hero.config.js
+++ src/twig/components/hero/hero.config.js
@@ -2,43 +2,72 @@ module.exports = {
   title: "Hero",
   status: "wip",
   context: {
-    heading: 'This is a Hero',
-    subheading: 'The most familiar component',
-    paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi volutpat ultricies consectetur. Duis vel ligula non nisi dignissim commodo vel dignissim nibh.</p>',
-    buttons: [
-      {
-        button: {
-          title: 'Active',
-          url: 'https://welcometruth.com/'
-        }
-      },
-      {
-        button: {
-          title: 'Active',
-          url: 'https://welcometruth.com/'
-        }
-      }
-    ],
+    heading: 'We’re on your team.',
+    paragraphs: '<p>Everyone levels up when adult learners go back to school. </p>',
     image: {
-      srcset: 'https://via.placeholder.com/960x540',
-      src: 'https://via.placeholder.com/960x540',
+      srcset: 'https://placehold.co/960x540',
+      src: 'https://placehold.co/960x540',
       alt: 'Test alt',
+    },
+    video: {
+      id: '86jwyC1kFDk',
     },
   },
   variants: [
     {
-      name: 'With Loop',
+      name: 'Reversed',
       context: {
-        loop: 'https://experience.brynathyn.edu/wp-content/themes/bryn-athyn/dist/videos/videoplayback2.mp4',
-      },
+        reverse: true,
+      }
     },
     {
-      name: 'With Video',
+      name: 'Parallel',
       context: {
-        video: {
-          id: '86jwyC1kFDk',
-        },
-      },
+        type: 'parallel',
+        tags: true,
+        subheading: 'Optional Heading',
+        heading: 'Headline Lorem Ipsum.',
+        paragraphs: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec.</p>',
+        buttons: [
+          {
+            button: {
+              title: 'Button text',
+              url: '/'
+            }
+          },
+          {
+            button: {
+              title: 'Button text',
+              url: '/'
+            }
+          }
+        ]
+      }
+    },
+    {
+      name: 'Reverse Parallel',
+      context: {
+        type: 'parallel',
+        reverse: true,
+        tags: true,
+        subheading: 'Optional Heading',
+        heading: 'Headline Lorem Ipsum.',
+        paragraphs: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec.</p>',
+        buttons: [
+          {
+            button: {
+              title: 'Button text',
+              url: '/'
+            }
+          },
+          {
+            button: {
+              title: 'Button text',
+              url: '/'
+            }
+          }
+        ]
+      }
     }
   ],
 }
--- src/twig/components/video/Video.vue
+++ src/twig/components/video/Video.vue
@@ -7,7 +7,40 @@
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+import YTPlayer from 'yt-player';
+
+const video = ref<HTMLElement | null>(null);
+const active = ref<boolean>(false);
+const player = ref<any>(null);
+
+const props = defineProps({
+  id: {
+      type: String,
+      required: true,
+    },
+});
+
+const setActive = () => {
+  active.value = true;
+
+  player.value.play();
+}
+
+const toggleActive = () => {
+  active.value = !active.value;
+
+  active.value ? player.value.play() : player.value.pause();
+}
+
+onMounted(() => {
+  player.value = new YTPlayer(video.value.querySelector('.video__iframe'));
+
+  player.value.load(props.id);
+});
+</script>
+
+<!-- <script>
 import YTPlayer from 'yt-player';
 
 export default {
@@ -41,4 +74,4 @@ export default {
     }
   },
 }
-</script>
+</script> -->
