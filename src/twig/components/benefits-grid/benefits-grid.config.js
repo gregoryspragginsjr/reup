c1efa0320084f7c286b280cf1b634bbe3b24fbc4
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2026-05-09 10:44:40 -0400

    Benefits grid added.

---
+++ src/twig/components/benefits-grid/benefits-grid.config.js
@@ -0,0 +1,78 @@
+module.exports = {
+  title: 'Benefits Grid',
+  status: 'wip',
+  context: {
+    subheading: 'Optional heading',
+    tags: true,
+    heading: 'Optional H2 Lorem Ipsum Dolor',
+    paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur condimentum mauris orci, sed tincidunt mauris porta convallis. Proin id nunc porttitor tortor pretium interdum.</p>',
+    buttons: [
+      {
+        button: {
+          title: 'Button text',
+          url: '/'
+        }
+      },
+      {
+        button: {
+          title: 'Button text',
+          url: '/'
+        }
+      }
+    ],
+    items: [
+      {
+        subheading: 'Eyebrow text',
+        heading: 'Feature/Benefit Heading',
+        paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+        buttons: [
+          {
+            button: {
+              title: 'Button text',
+              url: '/'
+            }
+          }
+        ]
+      },
+      {
+        subheading: 'Eyebrow text',
+        heading: 'Feature/Benefit Heading',
+        paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+        buttons: [
+          {
+            button: {
+              title: 'Button text',
+              url: '/'
+            }
+          }
+        ]
+      },
+      {
+        subheading: 'Eyebrow text',
+        heading: 'Feature/Benefit Heading',
+        paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+        buttons: [
+          {
+            button: {
+              title: 'Button text',
+              url: '/'
+            }
+          }
+        ]
+      },
+      {
+        subheading: 'Eyebrow text',
+        heading: 'Feature/Benefit Heading',
+        paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+        buttons: [
+          {
+            button: {
+              title: 'Button text',
+              url: '/'
+            }
+          }
+        ]
+      },
+    ]
+  }
+}
---
+++ src/twig/components/grids-section/grids-section.config.js
@@ -0,0 +1,304 @@
+module.exports = {
+  title: 'Grids Section',
+  status: 'wip',
+  context: {
+    subheading: 'Optional Heading',
+    heading: 'Headline Lorem Ipsum Dolor Sit Amet Conse Adipisci Dolor Sit.',
+    paragraphs: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus.</p>',
+    buttons: [
+      {
+        button: {
+          title: 'Button text',
+          url: '/'
+        }
+      },
+      {
+        button: {
+          title: 'Button text',
+          url: '/'
+        }
+      }
+    ],
+  },
+  variants: [
+    {
+      name: 'Section with Benefits Grid',
+      context: {
+        items: [
+          {
+            acf_fc_layout: 'benefits-grid',
+            subheading: 'Optional heading',
+            tags: true,
+            heading: 'Optional H2 Lorem Ipsum Dolor',
+            paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur condimentum mauris orci, sed tincidunt mauris porta convallis. Proin id nunc porttitor tortor pretium interdum.</p>',
+            buttons: [
+              {
+                button: {
+                  title: 'Button text',
+                  url: '/'
+                }
+              },
+              {
+                button: {
+                  title: 'Button text',
+                  url: '/'
+                }
+              }
+            ],
+            items: [
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+            ]
+          },
+          {
+            acf_fc_layout: 'benefits-grid',
+            items: [
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+            ]
+          }
+        ],
+      }
+    },
+    {
+      name: 'Indigo Section with Benefits Grid',
+      context: {
+        background: 'indigo',
+        items: [
+          {
+            acf_fc_layout: 'benefits-grid',
+            subheading: 'Optional heading',
+            tags: true,
+            heading: 'Optional H2 Lorem Ipsum Dolor',
+            paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur condimentum mauris orci, sed tincidunt mauris porta convallis. Proin id nunc porttitor tortor pretium interdum.</p>',
+            buttons: [
+              {
+                button: {
+                  title: 'Button text',
+                  url: '/'
+                }
+              },
+              {
+                button: {
+                  title: 'Button text',
+                  url: '/'
+                }
+              }
+            ],
+            items: [
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+            ]
+          },
+          {
+            acf_fc_layout: 'benefits-grid',
+            items: [
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+              {
+                subheading: 'Eyebrow text',
+                heading: 'Feature/Benefit Heading',
+                paragraphs: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit nunc efficitur, turpis ut tincidunt.</p>',
+                buttons: [
+                  {
+                    button: {
+                      title: 'Button text',
+                      url: '/'
+                    }
+                  }
+                ]
+              },
+            ]
+          }
+        ],
+      }
+    }
+  ]
+}
