001f4fd81fb094175b1b77f40fa33294a0514cff
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2026-05-12 18:18:12 -0400

    List accordion default added

---
+++ src/twig/components/list-accordion/list-accordion.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  title: "Hero",
+  status: "wip",
+  context: {
+    image: {
+      srcset: 'https://placehold.co/530x370',
+      src: 'https://placehold.co/530x370',
+      alt: 'Test alt',
+    },
+    display_heading: 'Reup<br/>Headline lorem ipsu dolor sit amet.',
+    display_paragraphs: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus. Lorem ipsum dolor sit amet, consecte adipiscing elit vestibulum orci est, auctor nec neque non. Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus. Lorem ipsum dolor sit amet, consecte adipiscing elit vestibulum orci est, auctor nec neque non.</p>',
+    subheading: 'Optional heading',
+    heading: 'Headline Lorem Ipsum',
+    paragraphs: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus. Lorem ipsum dolor sit amet, consecte adipiscing elit vestibulum orci est, auctor nec neque non.</p>',
+    buttons: [
+      {
+        button: {
+          title: 'Button text',
+          url: '/'
+        }
+      },
+      {
+        button: {
+          title: 'Button text',
+          url: '/'
+        }
+      }
+    ],
+    panels: [
+      {
+        heading: 'Headline Lorem Ipsum',
+        content: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus. Lorem ipsum dolor sit amet, consecte adipiscing elit vestibulum orci est, auctor nec neque non.</p>'
+      },
+      {
+        heading: 'Headline Lorem Ipsum',
+        content: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus. Lorem ipsum dolor sit amet, consecte adipiscing elit vestibulum orci est, auctor nec neque non.</p>'
+      },
+      {
+        heading: 'Headline Lorem Ipsum',
+        content: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus. Lorem ipsum dolor sit amet, consecte adipiscing elit vestibulum orci est, auctor nec neque non.</p>'
+      },
+      {
+        heading: 'Headline Lorem Ipsum',
+        content: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus. Lorem ipsum dolor sit amet, consecte adipiscing elit vestibulum orci est, auctor nec neque non.</p>'
+      },
+      {
+        heading: 'Headline Lorem Ipsum',
+        content: '<p>Lorem ipsum dolor sit amet, consecte adipiscing elit. Vestibulum orci est, auctor nec neque non, hendrerit luctus. Lorem ipsum dolor sit amet, consecte adipiscing elit vestibulum orci est, auctor nec neque non.</p>'
+      },
+    ]
+  }
+}
